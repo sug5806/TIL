374a9b9cf734d378581374e11b058efb56712f9b
sug5806 <sug5806@gmail.com>
2019-06-18 11:58:01 +0900

    feat: apply api

--- Django/api-basic2/photo/views.py
+++ Django/api-basic2/photo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+
+from django.views.generic import ListView
+from rest_framework import generics
+from .serializers import *
+
 # Create your views here.
+
+
+from .models import Photo
+
+
+class PhotoList(ListView):
+    model = Photo
+    template_name = 'photo/photo_list.html'
+
+
+class PhotoListAPI(generics.ListAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoListSerializer
