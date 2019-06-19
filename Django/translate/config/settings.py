98fbe6035efa6c436a82959648b1497bfe3a5cf5
sug5806 <sug5806@gmail.com>
2019-06-19 10:29:28 +0900

    feat: apply elastic beanstalk

--- Django/translate/config/settings.py
+++ Django/translate/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(9zagp3af$_3cybo46%*vhtbxbpli5(6lgs&8^qd***d0_t_*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
