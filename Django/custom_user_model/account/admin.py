090f114f5d40bca25fb1365e59f243e050e1625e
sug5806 <sug5806@gmail.com>
2019-06-24 11:15:10 +0900

    feat: additional commentary

--- Django/custom_user_model/account/admin.py
+++ Django/custom_user_model/account/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
 from .models import User
+from pprint import pprint
 
 # Register your models here.
 
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('message', 'profile')}),
-    )
-    # UserAdmin.fieldsets[1][1]['fields']+=('profile','message')
 
+    # fieldsets : 유저의 상세정보를 볼때 커스텀 필드를 추가 시킴
+    # 맨 마지막에 필드를 만들어 추가시킴
+    # fieldsets = UserAdmin.fieldsets + (
+    #     ('Additional Info', {'fields': ('message', 'profile')}),
+    # )
+
+    # 기존에 있는 필드에 추가를 시킨다
+    UserAdmin.fieldsets[3][1]['fields'] += ('profile', 'message')
+
+
+    # add_fieldsets : 유저를 만들때 커스텀 필드를 추가시킴
     UserAdmin.add_fieldsets += (
-        ('Additional Infoa', {'fields': ('message', 'profile')}),
+        ('Additional Info', {'fields': ('message', 'profile')}),
     )
 
 admin.site.register(User, CustomUserAdmin)
