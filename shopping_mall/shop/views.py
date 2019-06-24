e2418a89284b6af191fda253bb4555d9a93697f9
sug5806 <sug5806@gmail.com>
2019-06-24 16:18:15 +0900

    feat: create ProductList View & Product_list template

--- shopping_mall/shop/views.py
+++ shopping_mall/shop/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product, Category
+
+from django.views.generic import ListView
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'shop/product_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if 'slug' in self.kwargs:
+            category = Category.objects.filter(slug=self.kwargs['slug'])
+            if category.exists():
+                queryset = queryset.filter(category=category[0])
+            else:
+                queryset = queryset.none()
+
+        return queryset
