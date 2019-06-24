4383b1ee8b806b4205a873d6b0e83fa9287ceded
sug5806 <sug5806@gmail.com>
2019-06-24 17:54:23 +0900

    feat: apply When click category print proucts list with sub category products

--- shopping_mall/shop/views.py
+++ shopping_mall/shop/views.py
@@ -11,18 +11,28 @@ class ProductList(ListView):
     template_name = 'shop/product_list.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        # pk = 1 최상위 카테고리
         categories = Category.objects.filter(parent_category=Category.objects.get(pk=1)).order_by('name')
-        print(categories)
         kwargs.update({'categories': categories})
         return super().get_context_data(**kwargs)
 
     def get_queryset(self):
+        # 상위 카테고리를 고르면 하위 카테고리 제품들이 한꺼번에 출력되도록 변경
         queryset = super().get_queryset()
         if 'slug' in self.kwargs:
             category = Category.objects.filter(slug=self.kwargs['slug'])
             if category.exists():
-                queryset = queryset.filter(category=category[0])
+                category |= self.get_category_list(category[0])
+                queryset = queryset.filter(category__in=category)
+                print("category :   ", category)
+                print(queryset)
             else:
                 queryset = queryset.none()
 
         return queryset
+
+    def get_category_list(self, category):
+        categories = category.sub_categories.all()
+        for category in categories:
+            categories |= self.get_category_list(category)
+        return categories
