b49da60ece73179e0426aa44c4fd7c9323568d96
sug5806 <sug5806@gmail.com>
2019-07-01 14:10:15 +0900

    feat: create bubble_sort

---
+++ Python/algorithm/sort/bubble_sort/bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+if __name__ == "__main__":
+    arr = [7, 2, 6, 1, 10]
+    bubble_sort(arr)
+    print(arr)
