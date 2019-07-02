834c81bad3f2b76ab3180bf535cc8e0fbea1f6e6
sug5806 <sug5806@gmail.com>
2019-07-02 17:59:30 +0900

    feat: ForeignKey Filtering 연습

---
+++ Django/FK/foriegn/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ForiegnConfig(AppConfig):
+    name = 'foriegn'
---
+++ Django/FK/foriegn/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, related_name='entrys', on_delete=models.CASCADE)
+    headline = models.CharField(max_length1=100)
+    pub_date = models.DateField(auto_now_add=True)
+    mod_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.headline
+
+
+
+
