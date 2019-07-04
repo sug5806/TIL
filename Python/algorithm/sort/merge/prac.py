6372debf5e32f50504f94176c6946230ec95c11f
sug5806 <sug5806@gmail.com>
2019-07-04 12:58:23 +0900

    feat: merge_sort 추가

--- Python/algorithm/sort/merge/prac.py
+++ Python/algorithm/sort/merge/prac.py
@@ -1,52 +1,47 @@
-import random
-import time
+# 특정 케이스를 만들고 그것에 대해 알고리즘을 만들어서 푼다
 
-def merge(li, start, end):
-    n = len(li)
-    if n <= 1:
+def merge(arr, start, mid, end):
+    left = start
+    right = mid+1
+    li = []
+
+    while left <= mid and right <= end:
+        if arr[left] <= arr[right]:
+            li.append(arr[left])
+            left += 1
+        else:
+            li.append(arr[right])
+            right += 1
+
+    # 만약 right가 남아있다면
+    while right <= end:
+        li.append(arr[right])
+        right += 1
+
+    # 만약 left가 남아있다면
+    while left <= mid:
+        li.append(arr[left])
+        left += 1
+
+    arr[start:end+1] = li
+
+
+def merge_sort(arr, start, end):
+    # base case
+    if start >= end:
         return
 
-    mid = (start+end) // 2
-    sub_li1 = li[:mid]
-    sub_li2 = li[mid:]
+    mid = (start+end)//2
 
-    merge(sub_li1, 0, len(sub_li1))
-    merge(sub_li2, 0, len(sub_li2))
-    li1 = 0
-    li2 = 0
-    o_li = 0
+    # divide 를 먼저한다
+    merge_sort(arr, start, mid)
+    merge_sort(arr, mid+1, end)
 
-    while li1 < len(sub_li1) and li2 < len(sub_li2):
-        if sub_li1[li1] < sub_li2[li2]:
-            li[o_li] = sub_li1[li1]
-            li1 += 1
-            o_li += 1
-        else:
-            li[o_li] = sub_li2[li2]
-            li2 += 1
-            o_li += 1
-    
-    while li1 < len(sub_li1):
-        li[o_li] = sub_li1[li1]
-        li1 += 1
-        o_li += 1
-
-    while li2 < len(sub_li2):
-        li[o_li] = sub_li2[li2]
-        li2 += 1
-        o_li += 1
+    # conquer
+    merge(arr, start, mid, end)
 
 
-
-if __name__=="__main__":
-    while True:
-        num_data=int(input('데이터 개수(종료:0):'))
-        if not num_data:
-            break
-        data=[random.randint(1, 100) for _ in range(num_data)]
-        start = time.time()
-        print(data)
-        merge(data, 0, len(data)-1)
-        print("실행 시간 : {:0.2f}".format(time.time()-start))
-        print(data)
-        
+if __name__ == "__main__":
+    arr = [7, 2, 6, 1, 10, 9, 12, 11, 8]
+    merge_sort(arr, 0, len(arr)-1)
+    print(arr)
