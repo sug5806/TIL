fdad8fd7f04ccf301558e130036fd60cc7d11824
sug5806 <sug5806@gmail.com>
2019-08-17 15:03:05 +0900

    refac: code refac

--- Python/algorithm/sort/selection_sort/normal.py
+++ Python/algorithm/sort/selection_sort/normal.py
@@ -1,27 +1,28 @@
 import random
 import time
 
+
 def selection_sort(li):
     n = len(li)
-    for i in range(n-1):
+    for i in range(n - 1):
         min_idx = i
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             if li[min_idx] > li[j]:
-                min_idx = j    
-        li[i] , li[min_idx] = li[min_idx], li[i]
+                min_idx = j
+        li[i], li[min_idx] = li[min_idx], li[i]
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     while True:
-        num_data=int(input('데이터 개수(종료:0):'))
+        num_data = int(input('데이터 개수(종료:0):'))
         if not num_data:
             break
         start = time.time()
-        data=[random.randint(1, 100) for _ in range(num_data)]
-        end = time.time()
-        elips = end - start
-        #print(data)
+        data = [random.randint(1, 100) for _ in range(num_data)]
+        print(data)
         selection_sort(data)
-        #print(data)
-        print("{:0.2f}".format(elips))  
+        print(data)
+        end = time.time()
+        elips = end-start
+        # print(data)
+        print("{:0.2f}".format(elips))
